b736ccf698a5aa41c41f9f4c3f7dcf996a815d43
Anthony-sys <69772936+Anthony-sys@users.noreply.github.com>
2023-10-20 21:50:02 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/bin/env/python3
+import socket
 host = input('enter your ip:') 
 port = input('enter your port:') 
 server = socket.socket()
-server.bind ((host, port)) 
-server.listen
+server.bind ((host, int(port))) 
+server.listen(5)
 conn, addr = server.accept() 
 conn.close
